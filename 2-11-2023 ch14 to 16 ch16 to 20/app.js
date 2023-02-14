a56ba0edf243c2c52ff6552f1670d95fb16ec6ac
Faaiz124 <121377490+Faaiz124@users.noreply.github.com>
2023-02-14 17:45:53 +0500

    Add files via upload
    assigment ch 14 to 16 and 16 to 20

---
+++ 2-11-2023 ch14 to 16 ch16 to 20/app.js
@@ -0,0 +1,287 @@
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// >>>>>>>>>>>>Question no 1<<<<<<<<<<
+// student_name = prompt(["student name"]);
+// console.log(student_name);
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// >>>>>>>>>>>>Question no 2<<<<<<<<<<
+// Array = prompt(["student name"])
+// console.log(Array);
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// >>>>>>>>>>>>Question no 3<<<<<<<<<<
+// let x = prompt([""]);
+// console.log(x);
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// >>>>>>>>>>>>Question no 4<<<<<<<<<<
+// let x = prompt(["1,2,3"]);
+// console.log(x);
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// >>>>>>>>>>>>Question no 5<<<<<<<<<<
+// let arr1 = [];
+// for (let i = 0; i < 5; i += 1) {
+//     arr1.push(true);
+// }
+// console.log(arr1);;
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// >>>>>>>>>>>>Question no 6<<<<<<<<<<
+// mix = [1, 2, 4, "red", "blue", "pink"]
+// console.log(mix)
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// >>>>>>>>>>>>Question no 7<<<<<<<<<<
+// quali = ['SSC', 'HSC', 'BCS',
+//     'BS', 'BCOM', 'MS', 'M. Phil', 'PhD'
+// ]
+// document.write(quali)
+
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// >>>>>>>>>>>>Question no 8<<<<<<<<<<
+// var name = ["student1 ", "student2", "student3"];
+// var copy = name.slice(0, 8);
+// console.log(copy)
+// var copy1 = name.slice(10, 18);
+// console.log(copy)
+// var copy2 = name.slice(19, 28);
+// console.log(copy)
+// document.write("Score of " + copy + " " + "is 320 persentage 64%");
+// document.write("<br />");
+// document.write("Score of " + copy1 + " " + "is 230 persentage 46%");
+// document.write("<br />");
+// document.write("Score of " + copy2 + " " + "is 480 persentage 96%")
+// document.write("<br />");
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 9 A <<<<<<<<<<
+// colour = ["red", " blue", "green"]
+// var add = prompt('');
+// colour.unshift(add)
+// console.log(colour)
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 9 B <<<<<<<<<<
+// colour = ["red", " blue", "green"]
+// var add = prompt('');
+// colour.push(add)
+// console.log(colour)
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 9 C <<<<<<<<<<
+// colour = ["red", " blue", "green"]
+// colour.unshift("pink", "blue")
+// console.log(colour)
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 9 D <<<<<<<<<<
+// colour = ["red", " blue", "green"]
+// colour.shift()
+// console.log(colour);
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 9 E <<<<<<<<<<
+// colour = ["red", " blue", "green"]
+// colour.pop()
+// console.log(colour);
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 9 F <<<<<<<<<<
+// colour = ["red", " blue", "green"]
+// var whadd = prompt("whear u want to add colour just type in number")
+// var add = prompt("what colour u want to get")
+// colour.splice(whadd, 0, add)
+// console.log(colour);
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 9 G <<<<<<<<<<
+// colour = ["red", " blue", "green"]
+// var whadd = prompt("type number which colour u want to delete")
+// var add = prompt("how many colour u want to delete just add one and enter")
+// var add2 = prompt("name of colour which u want to add")
+// colour.splice(whadd, add, add2)
+// console.log(colour);
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 10 <<<<<<<<<<
+// let arr = [12, 25, 31, 23, 75, 81, 1];
+// document.write("Source of student " + arr);
+// document.write("</br>");
+// document.write("Orderd Source of student " + arr.sort());
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 11 <<<<<<<<<<
+// city = ["karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// var get = city.slice(0, 2);
+// document.write(get)
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 12 <<<<<<<<<<
+// var arr = ["this", "is", "my", "cat"];
+// let arre = arr.join();
+
+// document.write(arre)
+
+// >>>>>>>>>>chapter 14 to 16 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 13 <<<<<<<<<<
+// array = [];
+// array.push("first");
+// array.push("secound");
+// array.push("third");
+// document.write(array)
+// >>>>>>>>>>>>>>>>>>>>NEW CHAPTER <<<<<<<<<<<<<<<<<<<<<<<
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 1 <<<<<<<<<<
+// var twoD = [
+//     [],
+//     []
+// ];
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 2 <<<<<<<<<<
+// var twoD = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+// console.log(twoD[2][2]);
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 3 <<<<<<<<<<
+// for (let i = 1; i <= 10; i++) {
+//     // display the result
+//     document.write(i);
+// }
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 4 <<<<<<<<<<
+// var number = prompt();
+
+// //creating a multiplication table
+// for (let i = 1; i <= 10; i++) {
+
+//     // multiply i with number
+//     var result = i * number;
+
+//     // display the result
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 4 b <<<<<<<<<<
+// var number = (2);
+
+// //creating a multiplication table
+// for (let i = 1; i <= 15; i++) {
+
+//     // multiply i with number
+//     var result = i * number;
+
+//     // display the result
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 5 <<<<<<<<<<
+// fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry']
+// fruits.slice(0, 1, 2, 3, 4)
+// document.write(fruits)
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 7 a <<<<<<<<<<
+// for (let i = 1; i <= 10; i++) {
+//     // display the result
+//     document.write(i);
+// }
+
+
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 6 b <<<<<<<<<<
+// num = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+// num.reverse()
+// document.write(num)
+
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 6 c <<<<<<<<<<
+// num = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+// num.even()
+// document.write(num)
+
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // // >>>>>>>>>>>>Question no 7 d <<<<<<<<<<
+// let getEvenNumbers = () => {
+//     const arr = [1, 2, 3, 4, 5, 6];
+
+//     for (num of arr) {
+//         if (num % 2 === 0) { // 0 for even numbers and 1 for odd numbers.
+//             document.writeln(num + "<br />");
+//         }
+//     }
+// }
+// getEvenNumbers();
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 6 e <<<<<<<<<<
+// let getoddNumbers = () => {
+//     const arr = [1, 2, 3, 4, 5, 6];
+
+//     for (num of arr) {
+//         if (num % 2 === 1) { // 0 for even numbers and 1 for odd numbers.
+//             document.writeln(num + "<br />");
+//         }
+//     }
+// }
+// getoddNumbers();
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 6 f <<<<<<<<<<
+// for (let i = 1; i <= 10; i++) {
+//     // display the result
+//     document.write(i + 'k ');
+// }
+
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 7 <<<<<<<<<<
+// var get = prompt('wellcome to abc hottel what do you want to order?');
+
+// if (get === 'biryani') {
+//     alert("your order at your door step in 15 ,mins");
+// } else {
+//     alert("this item is not availeble")
+// }
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 8 <<<<<<<<<<
+
+// var array = [3, 6, 2, 56, 32, 5, 89, 32];
+// var largest = 0;
+
+// for (i = 0; i < array.length; i++) {
+//     if (array[i] > largest) {
+//         largest = array[i];
+//     }
+// }
+// console.log(largest);
+
+// >>>>>>>>>>chapter 17 to 20 <<<<<<<<<<<<<
+// // >>>>>>>>>>>>Question no 10 <<<<<<<<<<
+// var number = (5);
+
+// //creating a multiplication table
+// for (let i = 1; i <= 20; i++) {
+
+//     // multiply i with number
+//     var result = i * number;
+
+//     // display the result
+//     console.log(`${number} * ${i} = ${result}`);
+// }
